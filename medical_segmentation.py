afd5e3cfca6d9f8b96e4771232b10a932ffd07c2
phoenix-ZY <103630882+phoenix-ZY@users.noreply.github.com>
2023-05-24 23:16:44 +0800

    Create medical_segmentation.py

---
+++ medical_segmentation.py
@@ -0,0 +1,138 @@
+#coding=utf-8
+import argparse
+import os
+import time
+import logging
+import random
+import numpy as np
+from collections import OrderedDict
+
+import torch
+import torch.optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from torch.utils.data import DataLoader
+# from torch.utils.tensorboard import SummaryWriter
+
+import mmformer
+from data.transforms import *
+from data.datasets_nii import Brats_loadall_nii, Brats_loadall_test_nii
+from data.data_utils import init_fn
+from utils import Parser,criterions
+from utils.parser import setup 
+from utils.lr_scheduler import LR_Scheduler, record_loss, MultiEpochsDataLoader 
+from predict import AverageMeter, test_softmax
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-batch_size', '--batch_size', default=1, type=int, help='Batch size')
+parser.add_argument('--datapath', default=None, type=str)
+parser.add_argument('--dataname', default='BRATS2018', type=str)
+parser.add_argument('--savepath', default=None, type=str)
+parser.add_argument('--resume', default=None, type=str)
+parser.add_argument('--pretrain', default=None, type=str)
+parser.add_argument('--lr', default=2e-4, type=float)
+parser.add_argument('--weight_decay', default=1e-4, type=float)
+parser.add_argument('--num_epochs', default=1000, type=int)
+parser.add_argument('--iter_per_epoch', default=150, type=int)
+parser.add_argument('--region_fusion_start_epoch', default=0, type=int)
+parser.add_argument('--seed', default=1024, type=int)
+path = os.path.dirname(__file__)
+
+## parse arguments
+args = parser.parse_args()
+setup(args, 'training')
+args.train_transforms = 'Compose([RandCrop3D((128,128,128)), RandomRotion(10), RandomIntensityChange((0.1,0.1)), RandomFlip(0), NumpyType((np.float32, np.int64)),])'
+args.test_transforms = 'Compose([NumpyType((np.float32, np.int64)),])'
+
+ckpts = args.savepath
+os.makedirs(ckpts, exist_ok=True)
+
+###tensorboard writer
+# writer = SummaryWriter(os.path.join(args.savepath, 'summary'))
+
+###modality missing mask
+masks = [[False, False, False, True], [False, True, False, False], [False, False, True, False], [True, False, False, False],
+         [False, True, False, True], [False, True, True, False], [True, False, True, False], [False, False, True, True], [True, False, False, True], [True, True, False, False],
+         [True, True, True, False], [True, False, True, True], [True, True, False, True], [False, True, True, True],
+         [True, True, True, True]]
+masks_torch = torch.from_numpy(np.array(masks))
+mask_name = ['t2', 't1c', 't1', 'flair', 
+            't1cet2', 't1cet1', 'flairt1', 't1t2', 'flairt2', 'flairt1ce',
+            'flairt1cet1', 'flairt1t2', 'flairt1cet2', 't1cet1t2',
+            'flairt1cet1t2']
+
+def main():
+    ##########setting seed
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+
+    ##########setting models
+    if args.dataname in ['BRATS2021', 'BRATS2020', 'BRATS2018']:
+        num_cls = 4
+    elif args.dataname == 'BRATS2015':
+        num_cls = 5
+    else:
+        print ('dataset is error')
+        exit(0)
+    model = mmformer.Model(num_cls=num_cls)
+    model = model.cuda()
+    # model = torch.nn.DataParallel(model).cuda()
+
+    ##########Setting learning schedule and optimizer
+    lr_schedule = LR_Scheduler(args.lr, args.num_epochs)
+    train_params = [{'params': model.parameters(), 'lr': args.lr, 'weight_decay':args.weight_decay}]
+    optimizer = torch.optim.Adam(train_params,  betas=(0.9, 0.999), eps=1e-08, amsgrad=True)
+
+    ##########Setting data
+    if args.dataname in ['BRATS2020', 'BRATS2015']:
+        train_file = 'train.txt'
+        test_file = 'test.txt'
+    elif args.dataname == 'BRATS2018':
+        ####BRATS2018 contains three splits (1,2,3)
+        train_file = 'train3.txt'
+        test_file = 'test3.txt'
+
+    logging.info(str(args))
+    train_set = Brats_loadall_nii(transforms=args.train_transforms, root=args.datapath, num_cls=num_cls, train_file=train_file)
+    test_set = Brats_loadall_test_nii(transforms=args.test_transforms, root=args.datapath, test_file=test_file)
+    train_loader = MultiEpochsDataLoader(
+        dataset=train_set,
+        batch_size=args.batch_size,
+        num_workers=8,
+        pin_memory=True,
+        shuffle=True,
+        worker_init_fn=init_fn)
+    iter_per_epoch = len(train_loader)
+    train_iter = iter(train_loader)
+    for epoch in range(args.num_epochs):
+        step_lr = lr_schedule(optimizer, epoch)
+        # writer.add_scalar('lr', step_lr, global_step=(epoch+1))
+        b = time.time()
+        for i in range(iter_per_epoch):
+            step = (i+1) + epoch*iter_per_epoch
+            ###Data load
+            try:
+                data = next(train_iter)
+            except:
+                train_iter = iter(train_loader)
+                data = next(train_iter)
+            x, target, mask = data[:3]
+            x = x.cuda(non_blocking=True)
+            target = target.cuda(non_blocking=True)
+            mask = mask.cuda(non_blocking=True)
+
+            model.is_training = False
+            model(x, mask)
+            # fuse_pred = model(x, mask)
+            break
+            
+        break
+if __name__ == '__main__':
+    main()
+    print('finished')
